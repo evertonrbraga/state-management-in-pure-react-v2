29788ca9f34f0aa1073ff80b5c119e0dc416ee0f
Everton Braga <evertonr.braga@gmail.com>
2020-10-31 23:58:58 -0300

    Class version working

--- simple-counter-react-state-master/src/index.js
+++ simple-counter-react-state-master/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import HooksCounter from './HooksCounter';
-// import ClassCounter from './ClassCounter';
+// import HooksCounter from './HooksCounter';
+import ClassCounter from './ClassCounter';
 
 import './styles.scss';
 
@@ -10,8 +10,8 @@ const Application = () => {
   return (
     <main className="Application">
       <section className="Counters">
-        <HooksCounter max={15} step={5} />
-        {/* <ClassCounter max={15} step={5} /> */}
+        {/* <HooksCounter max={15} step={5} /> */}
+        <ClassCounter max={15} step={5} />
       </section>
     </main>
   );

04b2411a1efa5a953100d1b22d7497a2cd18e60d
Everton Braga <evertonr.braga@gmail.com>
2020-11-04 23:09:42 -0300

    Custom hook function

--- simple-counter-react-state-master/src/HooksCounter.js
+++ simple-counter-react-state-master/src/HooksCounter.js
@@ -11,7 +11,19 @@ const storeStateInLocalStorage = (count) => {
   console.log(localStorage);
 };
 
-const useLocalStorage = () => {};
+const useLocalStorage = (initialState, key) => {
+  const get = () => {
+    const storage = localStorage.getItem(key);
+    if (storage) return JSON.parse(storage)[value];
+    return initialState;
+  };
+
+  const [value, setValue] = useState(get());
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify({ value }));
+  });
+};
 
 const HooksCounter = ({ max, step }) => {
   const [count, setCount] = useState(getStateFromLocalStorage);

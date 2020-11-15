d2660eed1675a2b89c2bd55746d608b6e15a520d
Everton Braga <evertonr.braga@gmail.com>
2020-11-14 23:29:10 -0300

    React memo and useCallback

--- grudges-react-state/src/Application.js
+++ grudges-react-state/src/Application.js
@@ -1,4 +1,4 @@
-import React, { useState, useReducer } from 'react';
+import React, { useState, useReducer, useCallback } from 'react';
 
 import id from 'uuid/v4';
 
@@ -30,25 +30,30 @@ const reducer = (state, action) => {
 const Application = () => {
   const [grudges, dispatch] = useReducer(reducer, initialState);
 
-  const addGrudge = ({ person, reason }) => {
-    dispatch({
-      type: GRUDGE_ADD,
-      payload: {
-        person,
-        reason,
-        forgiven: false,
-        id: id(),
-      },
-    });
-    console.log(grudges);
-  };
+  const addGrudge = useCallback(
+    ({ person, reason }) => {
+      dispatch({
+        type: GRUDGE_ADD,
+        payload: {
+          person,
+          reason,
+          forgiven: false,
+          id: id(),
+        },
+      });
+    },
+    [dispatch]
+  );
 
-  const toggleForgiveness = (id) => {
-    dispatch({
-      type: GRUDGE_FORGIVE,
-      payload: id,
-    });
-  };
+  const toggleForgiveness = useCallback(
+    (id) => {
+      dispatch({
+        type: GRUDGE_FORGIVE,
+        payload: id,
+      });
+    },
+    [dispatch]
+  );
 
   return (
     <div className='Application'>
--- grudges-react-state/src/NewGrudge.js
+++ grudges-react-state/src/NewGrudge.js
@@ -4,28 +4,30 @@ const NewGrudge = ({ onSubmit }) => {
   const [person, setPerson] = useState('');
   const [reason, setReason] = useState('');
 
-  const handleChange = event => {
+  const handleChange = (event) => {
     event.preventDefault();
     onSubmit({ person, reason });
   };
 
+  console.log('new grudge');
+
   return (
-    <form className="NewGrudge" onSubmit={handleChange}>
+    <form className='NewGrudge' onSubmit={handleChange}>
       <input
-        className="NewGrudge-input"
-        placeholder="Person"
-        type="text"
+        className='NewGrudge-input'
+        placeholder='Person'
+        type='text'
         value={person}
-        onChange={event => setPerson(event.target.value)}
+        onChange={(event) => setPerson(event.target.value)}
       />
       <input
-        className="NewGrudge-input"
-        placeholder="Reason"
-        type="text"
+        className='NewGrudge-input'
+        placeholder='Reason'
+        type='text'
         value={reason}
-        onChange={event => setReason(event.target.value)}
+        onChange={(event) => setReason(event.target.value)}
       />
-      <input className="NewGrudge-submit button" type="submit" />
+      <input className='NewGrudge-submit button' type='submit' />
     </form>
   );
 };
